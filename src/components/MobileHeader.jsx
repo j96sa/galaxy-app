7f6cfa3a7e10504103e9397aba0f585a97347173
Josue Paulo Antuan <josue96sa@gmail.com>
2022-01-04 22:02:24 +0100

    menu mobile ok/:funcionalidad de dar tocar fuera del menu y que se cierre tambien 22:01--04/01/22

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/header/mobile-header.css"
 import logo from "../assets/starter-code/shared/logo.svg";
 import hamburguesa from "../assets/starter-code/shared/icon-hamburger.svg";
 import close from "../assets/starter-code/shared/icon-close.svg";
+import { Link } from 'react-router-dom';
 
 
 
 export default function MobileHeader() {
+    const [actNavBar, setActNavBar] = useState(false);
+
     return (
+        <>
         <div className='mh'>
             <img src={logo} alt="logo" />
-            <img src={hamburguesa} alt="menu" />
+            <img onClick={()=>setActNavBar(true)} src={hamburguesa} alt="menu" />
         </div>
+
+        <div className='placeholder'>
+            <div className={actNavBar ?"nav-bar active" :"nav-bar"}>
+                <img onClick={()=>setActNavBar(false)} src={close} alt="close"/>
+                <section className="nav-links">
+                    <Link to="/"><span>00 </span>HOME</Link>
+                    <Link to="/"><span>01 </span>DESTINATION</Link>
+                    <Link to="/"><span>02 </span>CREW</Link>
+                    <Link to="/"><span>03 </span>TECHNOLOGY</Link>
+                </section>
+            </div>
+        </div>
+        </>
     )
 }
